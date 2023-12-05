0bb9dfa935bc8f2532e8b2a9f13e64b23870cb61
geetkarthi <49567491+geetkarthi@users.noreply.github.com>
2023-12-05 13:04:37 -0500

    Update app.py

--- app.py
+++ app.py
@@ -217,7 +217,7 @@ elif selectbox_a == 'League position through a season':
         l = new_one.iloc[[selectbox3 - 1]]
         l = l.T
         a = l.plot()
-        st.pyplot(a.figure(figsize = (50, 8)))
+        st.pyplot(a.figure)
         
     elif (selectbox2 == 'EPL') or (selectbox2 == 'Ligue 1') or (selectbox2 == 'Serie A') or (selectbox2 == 'La Liga'):
         a = [[(0, 0, 0, 0) for i in range(38)] for i in range(20)]
